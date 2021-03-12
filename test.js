29555e8cd59138d2ee296572f0ff5f8815495511
DxxxxY <introsdream@gmail.com>
2021-03-12 09:14:22 -0500

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-let text = "Zisled has invited you to join their party!"
-
-if (text.includes("]")) {
-    console.log(text.substring(text.indexOf("]") + 2, text.indexOf("has") - 1))
-} else {
-    console.log(text.substring(0, text.indexOf("has") - 1))
-}
